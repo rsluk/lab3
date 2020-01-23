11cd4c9e16bb12ee7e059f6500b8169081a272bc
Rachel <rsluk@ucsd.edu>
2020-01-23 12:18:30 -0800

    Node.js edit

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,17 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
+exports.view = function(req, res){/*
   res.render('index', {
   	'name': '',
-  });
+  });*/
+  
+  var nameToDisplay = req.params.userName;
+  if (nameToDisplay == undefined){
+      nameToDisplay = 'World';
+   }
+   console.log("Name is " + nameToDisplay);
+   res.render('index', {
+        'name': nameToDisplay,
+        });
 };
